7ffedf3622653dc792a91f709daadb4e4cf700ea
Optimus ged <gedeonyalakuhanda@gmail.com>
2025-06-18 17:56:48 +0200

    Improve admin sidebar and dashboard

--- app/components/sidebar/AdminSidebar.tsx
+++ app/components/sidebar/AdminSidebar.tsx
@@ -13,32 +13,44 @@ import Image from "next/image";
 import Link from "next/link";
 import { JSX, useState } from "react";
 import SideBarWrapper from "./SideBarWrapper";
+import { usePathname } from "next/navigation";
+import classNames from "classnames";
 
-export default function SuperAdminSidebar() {
+export default function AdminSidebar() {
   const [open, setOpen] = useState(false);
   const [usersMenuOpen, setUsersMenuOpen] = useState(false);
+  const currentPath = usePathname();
 
   return (
     <SideBarWrapper>
-      <ul className="flex flex-col space-y-2 mt-2 px-4">
-        <li>
-          <Link
-            href="/"
-            className="block py-2 px-3 rounded hover:bg-gray-200 transition"
-          >
-            <div className="flex gap-1 items-center">
-              <House size={15} />
-              Dashboard
-            </div>
-          </Link>
-        </li>
-        <span className="flex md:py-1 px-3 border-t border-gray-100 text-xs text-gray-400">
-          People
-        </span>
+      <ul
+        className="bg-[#004484] rounded-lg mt-4 text-[#C1C1C1] p-4 flex flex-col gap-4
+          "
+      >
+        {sideBarLinks.map((link) => (
+          <li key={link.label}>
+            <Link
+              href={link.href} //
+              className={classNames({
+                "block py-2 px-3 rounded hover:bg-gray-200 hover:text-[#002648] transition":
+                  true,
+                "bg-gray-200 text-[#002648] ": currentPath === link.href,
+              })}
+            >
+              <div className="flex gap-1 items-center ">
+                {link.icon}
+                {link.label}
+              </div>
+            </Link>
+          </li>
+        ))}
         <li>
           <div
             onClick={() => setUsersMenuOpen(!usersMenuOpen)}
-            className="block py-2 px-3 rounded hover:bg-gray-200 transition cursor-pointer"
+            className={classNames({
+              "block py-2 px-3 rounded hover:bg-gray-200 hover:text-[#002648] transition":
+                true,
+            })}
           >
             <div className="flex gap-1 items-center justify-between">
               <div className="flex gap-1 items-center">
@@ -56,7 +68,7 @@ export default function SuperAdminSidebar() {
             <div className="ml-6 mt-1 space-y-1">
               <Link
                 href="/pages/user/business/"
-                className="py-1 px-3 rounded hover:bg-gray-200 transition text-sm flex items-center gap-2"
+                className="py-1 px-3 rounded hover:bg-gray-200 hover:text-[#002648] transition text-sm flex items-center gap-2"
               >
                 <Building2 size={14} />
                 Businesses
@@ -69,16 +81,8 @@ export default function SuperAdminSidebar() {
   );
 }
 
-
 const sideBarLinks: {
   label: string;
   href: string;
   icon: JSX.Element;
-}[] = [
-  { label: "Dashboard", href: "/", icon: <House size={15} /> },
-  {
-    label: "Survey",
-    href: "/pages/user/survey",
-    icon: <File size={15} />,
-  }
-];
+}[] = [{ label: "Dashboard", href: "/", icon: <House size={15} /> }];
