f16973be4f1dfbf944b1ede0ea32cc6f46bee8ae
Optimus ged <gedeonyalakuhanda@gmail.com>
2025-06-18 17:47:27 +0200

    Improve sidebar and super admin dashboard

--- app/components/dashboard/SuperAdminDashboard.tsx
+++ app/components/dashboard/SuperAdminDashboard.tsx
@@ -4,91 +4,60 @@ import { useState } from "react";
 import DashboardCharts from "../charts/DashboardCharts";
 import DashboardSkeleton from "../skeleton/Dashboardskeleton";
 import FormPopup from "../pop-up/PopUpForm";
+import BusinessCard from "@/app/pages/user/business/components/BusinessCard";
 
 const SuperAdminDashboard = () => {
-	const [openKycVerification, setOpenKycVerification] = useState(false);
-	const user = useUser();
+  const [openKycVerification, setOpenKycVerification] = useState(false);
+  const user = useUser();
 
-	if (!user) {
-		return (
-			<DashboardSkeleton />
-		)
-	}
+  if (!user) {
+    return <DashboardSkeleton />;
+  }
 
-	return (
-		<div>
-
-			{openKycVerification && (
-				<FormPopup
-					setOpen={setOpenKycVerification}
-					title="KYC Verification"
-					method={"POST"}
-					endPoint="business-auth/upload-certificate-of-incorporation"
-					fields={[
-						{ name: "email", label: "", type: "hidden", required: true },
-						{
-							name: "certificateOfIncorporation",
-							label: "Certificate of Incorporation",
-							type: "file",
-							required: true,
-						},
-					]}
-					defaultValues={{ email: user?.email }}
-				/>
-			)}
-			<div className="w-full">
-				<p className="font-semibold text-md space-y-1">
-					Hi Esther, here’s your platform overview for today
-				</p>
-				<p className="text-sm">
-					All systems operational. Last sync: 10 mins ago
-				</p>
-			</div>
-			<div className="grid grid-cols-2 md:grid-cols-3 gap-4 mt-5">
-				<div className="border border-[#F7F7F7] bg-white py-2 px-3 rounded-md">
-					<h3 className="flex gap-1 items-center text-sm mb-2">
-						<UsersRound size={13} />
-						Total Users
-					</h3>
-					<div className="flex items-center gap-2 text-lg font-semibold text-gray-700">
-						<p>1,2340</p>
-						<div className=" text-[10px] text-[#0BAD2E] border border-[#0BAD2E] px-[1px] rounded">
-							10.2%
-						</div>
-					</div>
-					<p className="text-xs">Registered accross all roles</p>
-				</div>
-
-				<div className="border border-[#F7F7F7] bg-white py-2 px-3 rounded-md ">
-					<h3 className="flex gap-1 items-center text-sm mb-2">
-						<Banknote size={13} /> Revenue (This Month)
-					</h3>
-					<div className="flex items-center gap-2 text-lg font-semibold text-gray-700">
-						<p>₦1,845,200</p>
-						<div className=" text-[10px] text-[#0BAD2E] border border-[#0BAD2E] px-[1px] rounded">
-							10.2%
-						</div>
-					</div>
-					<p className="text-xs">Total from active campaigns</p>
-				</div>
-
-				<div className="border border-[#F7F7F7] bg-white py-2 px-3 rounded-md">
-					<h3 className="flex gap-1 items-center text-sm mb-2">
-						<CheckSquare size={13} /> <div className="hidden md:inline"></div>
-						Task completion time
-					</h3>
-					<div className="flex items-center gap-2 text-lg font-semibold text-gray-700">
-						<p>3m 42s</p>
-						<div className=" text-[10px] text-[#0BAD2E] border border-[#0BAD2E] px-[1px] rounded">
-							10.2%
-						</div>
-					</div>
-					<p className="text-xs">Across mobile & QR responses</p>
-				</div>
-			</div>
-			<DashboardCharts />
-		</div>
-	);
+  return (
+    <div>
+      {openKycVerification && (
+        <FormPopup
+          setOpen={setOpenKycVerification}
+          title="KYC Verification"
+          method={"POST"}
+          endPoint="business-auth/upload-certificate-of-incorporation"
+          fields={[
+            { name: "email", label: "", type: "hidden", required: true },
+            {
+              name: "certificateOfIncorporation",
+              label: "Certificate of Incorporation",
+              type: "file",
+              required: true,
+            },
+          ]}
+          defaultValues={{ email: user?.email }}
+        />
+      )}
+      <div className="w-full">
+        <p className="font-semibold text-md space-y-1">
+          Hi Esther, here’s your platform overview for today
+        </p>
+        <p className="text-sm">
+          All systems operational. Last sync: 10 mins ago
+        </p>
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-4 mt-5">
+        {[...Array(3)].map((_, index) => (
+          <BusinessCard
+            key={index}
+            title="Active Campaigns"
+            subTitle="Increase from last month"
+            value="67"
+            percentage="10.2%"
+            icon={<UsersRound />}
+            isHighLighted={index === 0}
+          />
+        ))}
+      </div>
+      <DashboardCharts />
+    </div>
+  );
 };
 
 export default SuperAdminDashboard;
