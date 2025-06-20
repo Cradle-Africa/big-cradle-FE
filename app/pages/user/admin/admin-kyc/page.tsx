8aea470112e6dd54f8c0e6c4d39673b55c343e35
Optimus ged <gedeonyalakuhanda@gmail.com>
2025-06-20 15:06:20 +0200

    Add layout to the admin kyc page

--- app/pages/user/admin/admin-kyc/page.tsx
+++ app/pages/user/admin/admin-kyc/page.tsx
@@ -1,51 +1,49 @@
-'use client'
-import React from 'react'
-import TableComponent from '@/app/components/table/TableComponent';
+"use client";
+import React from "react";
+import TableComponent from "@/app/components/table/TableComponent";
+import DashboardLayout from "@/app/DashboardLayout";
 
 const AdminKyc = () => {
+  const fields = [
+    { key: "email", label: "Email", className: "" },
+    { key: "userType", label: "User Type", className: "" },
+    { key: "firstName", label: "First Name", className: "" },
+    { key: "lastName", label: "Last Name", className: "" },
+    { key: "phoneNumber", label: "Phone Number", className: "" },
+    { key: "country", label: "Country", className: "" },
+    { key: "city", label: "City", className: "" },
+    { key: "state", label: "State", className: "" },
+    { key: "kycStatus", label: "Status", className: "" },
+    { key: "createdAt", label: "Created at", className: "" },
+  ];
 
-    const fields = [
-        { key: 'email', label: 'Email', className: '' },
-        { key: 'userType', label: 'User Type', className: '' },
-        { key: 'firstName', label: 'First Name', className: '' },
-        { key: 'lastName', label: 'Last Name', className: '' },
-        { key: 'phoneNumber', label: 'Phone Number', className: '' },
-        { key: 'country', label: 'Country', className: '' },
-        { key: 'city', label: 'City', className: '' },
-        { key: 'state', label: 'State', className: '' },
-        { key: 'kycStatus', label: 'Status', className: '' },
-        { key: 'createdAt', label: 'Created at', className: '' },
-    ]
+  const actionConfig = {
+    review: {
+      endPoint: "super-admin-auth/review-admin-kyc",
+      method: "POST",
+      payload: {},
+    },
+    view: {
+      endPoint: "",
+      method: "",
+      payload: {},
+    },
+  };
 
-    const actionConfig = {
-        review: {
-            endPoint: 'super-admin-auth/review-admin-kyc',
-            method: 'POST',
-            payload: {}
-        },
-        view: {
-            endPoint: '',
-            method: '',
-            payload: {}
-        },
+  return (
+    <DashboardLayout>
+      <TableComponent
+        title="Investors KYC"
+        endpoint="super-admin-auth/all-admin-kyc-review"
+        fields={fields}
+        breadcrumbs={{
+          parent: { path: "/Dashboard", label: "User" },
+          current: "Investors KYC",
+        }}
+        actionConfig={actionConfig}
+      />
+    </DashboardLayout>
+  );
+};
 
-    }
-
-    return (
-        <>
-            <TableComponent
-                title="Investors KYC"
-                endpoint="super-admin-auth/all-admin-kyc-review"
-                fields={fields}
-                breadcrumbs={{
-                    parent: { path: '/Dashboard', label: 'User' },
-                    current: 'Investors KYC'
-                }}
-                actionConfig={actionConfig}
-            />
-        </>
-
-    )
-}
-
-export default AdminKyc
+export default AdminKyc;
