22f60953f063315c16b090fa6b0fddf9f1ab3849
Optimus ged <gedeonyalakuhanda@gmail.com>
2025-06-24 11:35:42 +0200

    Implement survey navigation with questions area

---
+++ app/pages/survey/_components/ResponsesArea.tsx
@@ -0,0 +1,9 @@
+const ResponsesArea = () => {
+  return (
+    <div className="my-8">
+      <p className="text-4xl font-bold">Responses</p>
+    </div>
+  );
+};
+
+export default ResponsesArea;
---
+++ app/pages/survey/_components/SettingsArea.tsx
@@ -0,0 +1,9 @@
+const SettingsArea = () => {
+  return (
+    <div className="my-8">
+      <p className="text-4xl font-bold">Settings</p>
+    </div>
+  );
+};
+
+export default SettingsArea;
