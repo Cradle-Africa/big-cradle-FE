196849051fbb167c72ad73a94bcefcb184a62747
Optimus ged <gedeonyalakuhanda@gmail.com>
2025-06-26 10:27:04 +0200

    Display surveys by status

--- app/pages/survey/page.tsx
+++ app/pages/survey/page.tsx
@@ -8,24 +8,38 @@ import build_pipeline from "@/public/icons/build_pipeline.png";
 import { Plus } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import toast from "react-hot-toast";
-import Card from "./_components/SurveyCard";
+import Overview from "../flywheel/_components/Overview";
+import {
+  useFetchDataEntries,
+  useFetchPipelines,
+} from "../flywheel/_features/hook";
 import SurveysListArea from "./_components/SurveysListArea";
 import { statuses } from "./_components/SurveyStatus";
 import { useFetchSurvey, useVerifySurvey } from "./_features/hooks";
 import LoadingNewSurveyPage from "./new/loading";
+import { SurveyListItem } from "@/app/lib/type";
 
 const SurveyPage = () => {
   const [open, setOpen] = useState(false);
+  const searchParam = useSearchParams();
   const menuRef = useRef<HTMLDivElement>(null);
+  const router = useRouter();
   const searchParams = useSearchParams();
+  const [entriesPage] = useState(1);
+  const [entriesLimit] = useState(10);
+  // const [filteredSuveys, setFilteredSurveys] = useState<SurveyListItem[]>([]);
+
+  const surveyStatus = searchParam.get("status");
+
+  const [pipelinesPage] = useState(1);
+  const [pipelinesLimit] = useState(10);
 
   const user = getUser();
 
   const txRef = searchParams.get("tx_ref");
-
   const {
     mutateAsync: verifyPayment,
     isPending: isVerifing,
@@ -34,10 +48,17 @@ const SurveyPage = () => {
     axios,
   });
 
-  const {
-    data: surveysListResponse,
-    isLoading,
-  } = useFetchSurvey({ axios, businessUserId: user?.id ?? "", page: 1 });
+  const { data: surveysListResponse, isLoading } = useFetchSurvey({
+    axios,
+    businessUserId: user?.id ?? "",
+    page: 1,
+  });
+
+  const surveys: SurveyListItem[] = surveysListResponse?.survey || [];
+  const filteredSurveys = useMemo(() => {
+    if (!surveyStatus || surveyStatus === "All") return surveys;
+    return surveys.filter((survey) => survey.paymentStatus === surveyStatus);
+  }, [surveyStatus, surveys]);
 
   useEffect(() => {
     const handler = (e: MouseEvent) => {
@@ -65,6 +86,39 @@ const SurveyPage = () => {
       toast.error(`Error ---> ${error.message}`);
     }
   }, [txRef, verifyPayementFunc]);
+
+  const { isLoading: isLoadingDataPoints, data: dataPointsData } =
+    useFetchPipelines({
+      axios,
+      queryParams: {
+        page: pipelinesPage,
+        limit: pipelinesLimit,
+      },
+    });
+
+  const { data: dataEntries } = useFetchDataEntries({
+    axios,
+    queryParams: {
+      page: entriesPage,
+      limit: entriesLimit,
+    },
+  });
+
+  const dataentries = dataEntries?.data ?? [];
+  const paginationDataEntries = dataEntries?.pagination ?? {
+    page: 1,
+    limit: 10,
+    pages: 1,
+    total: 0,
+  };
+
+  const pipelines = dataPointsData?.dataPoint ?? [];
+  const paginationDataPoints = dataPointsData?.pagination ?? {
+    page: 1,
+    limit: 10,
+    pages: 1,
+    total: 0,
+  };
 
   useEffect(() => {
     if (isError) toast.error(`Error ---> An error occured`);
@@ -142,26 +196,39 @@ const SurveyPage = () => {
         </button>
       </div>
       {/* Build the cards area */}
-      <div className="flex gap-6 mt-8">
+      {/* <div className="flex gap-6 mt-8">
         {[...Array(3)].map((_, index) => (
           <Card key={`${index}`} isHighLighted={index === 0} />
         ))}
-      </div>
+      </div> */}
+      <Overview
+        pipelines={pipelines?.length}
+        dataentries={dataentries?.length}
+      />
 
       {/* Survey table  */}
       <div className="flex flex-col bg-white p-4 mt-8">
         <p className="font-bold text-black">Survey Table List</p>
         <div className="flex gap-4 my-4">
           {statuses.map((status) => (
+            // <SurveyStatusBadge
+            //   key={status}
+            //   isSelected={status === surveyStatus}
+            //   status={status}
+            //   onClick={() => router.push(`/pages/survey?status=${status}`)}
+            // />
             <SurveyStatus
               key={status}
-              isSelected={status === "All"}
+              isSelected={status === surveyStatus}
               status={status}
+              onClick={() =>
+                router.push(`/pages/survey?status=${status.toLowerCase()}`)
+              }
             />
           ))}
         </div>
         {/* <SurveyTable /> */}
-        <SurveysListArea data={surveysListResponse?.survey || []} />
+        <SurveysListArea data={filteredSurveys || []} />
       </div>
     </DashboardLayout>
   );
