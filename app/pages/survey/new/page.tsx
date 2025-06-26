ce826085a4e90f53aaa3957d1227c7221f537ec0
Optimus ged <gedeonyalakuhanda@gmail.com>
2025-06-26 18:05:25 +0200

    fix the new page issue

--- app/pages/survey/new/page.tsx
+++ app/pages/survey/new/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import DashboardLayout from "@/app/DashboardLayout";
 import { DataPointForm, SurveySchema } from "@/app/lib/type";
 import { surveySchema } from "@/app/lib/validationSchemas";
@@ -26,17 +27,6 @@ const NewSurveyPage = () => {
     field: [],
   });
 
-  const txRef = searchParams.get("tx_ref");
-  const {
-    mutateAsync: verifyPayment,
-    isPending: isVerifing,
-    data: paymentMadeData,
-    isSuccess: isVerifyPaymentSuccess,
-    isError,
-  } = useVerifySurveyPayment({
-    axios,
-  });
-
   const {
     register,
     watch,
@@ -54,88 +44,74 @@ const NewSurveyPage = () => {
     router.push(`/pages/survey/new?survey=survey-questions`);
   };
 
-  const filteredSurveys = useMemo(() => {
-    const surveys: SurveyListItem[] = surveysListResponse?.survey || [];
-    if (!surveyStatus || surveyStatus === "all") return surveys;
-    if (surveyStatus.toLowerCase() === "active") {
-      return surveys.filter((survey) => survey.isActive);
-    } else {
-      return surveys.filter((survey) => !survey.isActive);
-    }
-  }, [surveyStatus, surveysListResponse?.survey]);
+  // const { data : singleSurvey, isLoading: isLoadingSingleSurvey } = useFetchSingleSurvey({
+  //   axios,
+  //   surveyId: surveyId || "",
+  //   enabled: !!surveyId,
+  // });
 
   return (
     <DashboardLayout>
-      {open && (
-        <>
-          <div className="fixed inset-0 bg-black/40 z-40" />
-          <div
-            className="text-center bg-white px-5 py-5 md:px-8 md:py-8 rounded-lg w-82 md:w-full max-w-xl z-50 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-            ref={menuRef}
+      <div className="flex gap-4 items-center justify-center">
+        {/* Survey buttons area */}
+        {/* {surveyMenuList.map((survey: string) => (
+          <SurveyTabButton
+            onClick={() => router.push(`/pages/survey/new?survey=${survey}`)}
+            key={survey}
+            isSelected={paramSurvey === survey}
           >
-            <h2 className="text-gray-800 text-lg font-normal">
-              How would you like to collect your data?
-            </h2>
-            <p className="mt-5 text-gray-700">
-              Choose an integration method or build your own feedback tool
-            </p>
-            <div className="flex justify-between gap-5 mt-10">
-              <div className="w-1/2 px-6 py-6 bg-[#FCEBFF] rounded-lg cursor-pointer">
-                <div className="flex justify-center">
-                  <Image alt="api icon" src={api_icon} width={40} height={40} />
-                </div>
-                <p className="mt-3 text-md text-gray-800">Connect via API</p>
-                <p className="text-sm mt-3">
-                  Integrate with your existing apps, websites, or CRM
-                </p>
-              </div>
-
-              <div className="w-1/2 px-6 py-6 bg-[#E6E9FF] rounded-lg cursor-pointer">
-                <div className="flex justify-center">
-                  <Image
-                    alt="build icon"
-                    src={build_pipeline}
-                    width={40}
-                    height={40}
-                  />
-                </div>
-                <p className="mt-3 text-md text-gray-800">
-                  Build Custom Pipeline
-                </p>
-                <p className="text-sm mt-3">
-                  Use our form builder to create your own survey
-                </p>
-              </div>
-            </div>
-          </div>
-        </>
-      )}
-      <div className="flex justify-between">
-        <div className="flex flex-col gap-2">
-          <p className="font-medium text-black">Surveys</p>
-          <p>
-            View, manage, and track your survey tasks. Create new surveys to
-            collect insights in real time
-          </p>
-        </div>
-        <button
-          className="bg-[#3352FF] rounded-[8px] px-4 h-[36px] cursor-pointer"
-          //   onClick={() => setOpen(true)}
+            <p>{survey}</p>
+          </SurveyTabButton>
+        ))} */}
+        <SurveyTabButton
+          // onClick={() =>
+          //   router.push(`/pages/survey/new?survey=survey-name-and-description`)
+          // }
+          isSelected={paramSurvey === "survey-name-and-description"}
         >
-          <Link href="/pages/survey/new?survey=survey-name-and-description">
-            <div className="flex fgap2 items-center gap-2">
-              <Plus size={18} color="white" />
-              <span className="text-white">Create new survey</span>
+          <div className="flex gap-4 items-center">
+            <div className="rounded-full border-2 border-green-600 h-[20px] w-[20px] flex items-center justify-center">
+              <p>1</p>
             </div>
-          </Link>
-        </button>
+            <p>Survey name and description</p>
+          </div>
+        </SurveyTabButton>
+
+        <SurveyTabButton
+          // onClick={() =>
+          //   router.push(`/pages/survey/new?survey=survey-questions`)
+          // }
+          isSelected={paramSurvey === "survey-questions"}
+        >
+          <div className="flex gap-4 items-center">
+            <div className="rounded-full border-2 border-green-600 h-[20px] w-[20px] flex items-center justify-center">
+              <p>2</p>
+            </div>
+            <p>Survey questions</p>
+          </div>
+        </SurveyTabButton>
+
+        <SurveyTabButton
+          // onClick={() =>
+          //   router.push(`/pages/survey/new?survey=survey-questions`)
+          // }
+          isSelected={paramSurvey === "survey-payment"}
+        >
+          <div className="flex gap-4 items-center">
+            <div className="rounded-full border-2 border-green-600 h-[20px] w-[20px] flex items-center justify-center">
+              <p>3</p>
+            </div>
+            <p>Survey payment</p>
+          </div>
+        </SurveyTabButton>
+
+        {/* <SurveyTabButton
+          onClick={() => router.push(`/pages/survey/new?survey=Surveys list`)}
+          isSelected={false}
+        >
+          <div>Surveys list</div>
+        </SurveyTabButton> */}
       </div>
-      {/* Build the cards area */}
-      {/* <div className="flex gap-6 mt-8">
-        {[...Array(3)].map((_, index) => (
-          <SurveyCard key={`${index}`} isHighLighted={index === 0} />
-        ))}
-      </div> */}
 
       {/* Forms area */}
       <FormArea
