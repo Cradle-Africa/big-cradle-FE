2eed220c180eea46bfa924ce7badec92e3aeaf59
Optimus ged <gedeonyalakuhanda@gmail.com>
2025-06-26 07:51:16 +0200

    Survey test implementation

--- app/pages/survey/_components/SurveyNameAndDescription.tsx
+++ app/pages/survey/_components/SurveyNameAndDescription.tsx
@@ -1,42 +1,53 @@
-import { useState } from "react";
-import NewSurveyForm from "./NewSurveyQuestionsForm";
-import { useCreateSurveyForm } from "../_features/hooks";
+import ErrorMessage from "@/app/components/form/ErrorMessage";
+import { SurveySchema } from "@/app/lib/type";
+import { error } from "console";
 import { useRouter } from "next/navigation";
+import {
+  FieldErrors,
+  UseFormHandleSubmit,
+  UseFormRegister,
+} from "react-hook-form";
 
-const SurveyNameAndDescription = () => {
-  const {
-    register,
-    resetField,
-    watch,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-  } = useCreateSurveyForm();
+type Props = {
+  register: UseFormRegister<SurveySchema>;
+  // surveyName: string;
+  // surveyDescription: string;
+  errors: FieldErrors<SurveySchema>;
+  handleSubmit: UseFormHandleSubmit<SurveySchema>;
+  onSubmit: (data: SurveySchema) => void;
+};
+
+const SurveyNameAndDescription = ({
+  register,
+  // surveyName,
+  // surveyDescription,
+  errors,
+  handleSubmit,
+  onSubmit,
+}: Props) => {
   const router = useRouter();
 
-  const surveyName = watch("surveyName");
-  const surveyDescription = watch("surveyDescription");
-
   return (
-    <div className="my-8 flex flex-col gap-4 max-w-3xl mx-auto">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="my-8 flex flex-col gap-4 max-w-3xl mx-auto"
+    >
       <input
         {...register("surveyName")}
         placeholder="Survey name"
         className="w-full border border-gray-300 rounded-md px-3 py-2 outline-none bg-white"
-        required
       />
+      <ErrorMessage>{errors.surveyName?.message}</ErrorMessage>
       <textarea
         {...register("surveyDescription")}
         placeholder="Survey Description"
         className="w-full border border-gray-300 rounded-md px-3 py-2 outline-none bg-white"
-        required
       />
-      <button
-        onClick={() => router.push(`/pages/survey/new?survey=survey-questions`)}
-        className="bg-blue-600 rounded-md py-2 px-8 mr-auto"
-      >
+      <ErrorMessage>{errors.surveyDescription?.message}</ErrorMessage>
+      <button className="bg-blue-600 rounded-md py-2 px-8 mr-auto">
         <span className="text-white">Next</span>
       </button>
-    </div>
+    </form>
   );
 };
 
