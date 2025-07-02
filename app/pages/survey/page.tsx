94974ee23f1e39924380300552ab77706b8f847a
Optimus ged <gedeonyalakuhanda@gmail.com>
2025-07-02 10:23:19 +0200

    Remove create survey button for the super admin

--- app/pages/survey/page.tsx
+++ app/pages/survey/page.tsx
@@ -241,17 +241,20 @@ const SurveyPage = () => {
       <div className="w-full">
         <div className="flex justify-between">
           <p className="font-medium text-black">Surveys</p>
-          <button
-            className="flex bg-[#3352FF] rounded-md px-4 py-1 lg:py-2 cursor-pointer"
-            //   onClick={() => setOpen(true)}
-          >
-            <Link href="/pages/survey/new?survey=location-and-demographic">
-              <div className="flex items-center gap-2">
-                <Plus size={18} color="white" />
-                <span className="text-white">Create new survey</span>
-              </div>
-            </Link>
-          </button>
+
+          {user?.role === "business" && (
+            <button
+              className="flex bg-[#3352FF] rounded-md px-4 py-1 lg:py-2 cursor-pointer"
+              //   onClick={() => setOpen(true)}
+            >
+              <Link href="/pages/survey/new?survey=location-and-demographic">
+                <div className="flex items-center gap-2">
+                  <Plus size={18} color="white" />
+                  <span className="text-white">Create new survey</span>
+                </div>
+              </Link>
+            </button>
+          )}
         </div>
         <p className="mt-2">
           View, manage, and track your survey tasks. Create new surveys to
