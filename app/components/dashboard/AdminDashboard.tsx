2bda68b2e3529e3a5fd10362322cf6bd55cc31cf
Optimus ged <gedeonyalakuhanda@gmail.com>
2025-07-02 14:09:32 +0200

    Add kyc verification banner for the admin

--- app/components/dashboard/AdminDashboard.tsx
+++ app/components/dashboard/AdminDashboard.tsx
@@ -1,27 +1,40 @@
 import { DashboardMenu } from "@/app/lib/type";
+import axios from "@/app/lib/axios";
 import BusinessCard from "@/app/pages/user/business/_components/BusinessCard";
 import { getUser } from "@/app/utils/user/userData";
 import { ArrowDownUp, Database, Scan, UsersRound } from "lucide-react";
 import DashboardCharts from "../charts/DashboardCharts";
 import DashboardSkeleton from "../skeleton/Dashboardskeleton";
+import { useFetchMe } from "@/app/shared/_features/hooks";
+import KycVerification from "../KycVerification";
+import { useState } from "react";
 
 const BusinessDashboard = () => {
-  
-  const user = getUser();
-  if (!user) {
+  const [openBusinessKycVerification, setOpenBusinessKycVerification] =
+    useState(false);
+  const [openAdminKycVerification, setOpenAdminKycVerification] =
+    useState(false);
+  const { data: user, isLoading } = useFetchMe({ axios });
+
+  if (isLoading) {
     return <DashboardSkeleton />;
   }
 
   return (
     <div>
-      {/* <KycVerification
-        openBusinessKycVerification={openBusinessKycVerification}
-        setOpenBusinessKycVerification={setOpenBusinessKycVerification}
-        openAdminKycVerification={openAdminKycVerification}
-        setOpenAdminKycVerification={setOpenAdminKycVerification}
-        user={user}
-        kycReviewReason={user?.kycReviewReason || ""}
-      /> */}
+      {isLoading ? (
+        <div>
+          <p>Loading kwc verification... </p>
+        </div>
+      ) : (
+        <KycVerification
+          openBusinessKycVerification={openBusinessKycVerification}
+          setOpenBusinessKycVerification={setOpenBusinessKycVerification}
+          openAdminKycVerification={openAdminKycVerification}
+          setOpenAdminKycVerification={setOpenAdminKycVerification}
+          user={user?.data!}
+        />
+      )}
       <div className="grid grid-cols-2 md:grid-cols-3 gap-4 mt-5">
         {data.map((menu, index) => (
           <BusinessCard
