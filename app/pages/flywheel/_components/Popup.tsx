a28b907d1ffe994894d9c24ae9bbbbfec77a60a5
philippetsongo <philippetsongo90@gmail.com>
2025-07-11 19:28:54 +0200

    integration of the data flywheen analitics, fix the the download to pdf

--- app/pages/flywheel/_components/Popup.tsx
+++ app/pages/flywheel/_components/Popup.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ArrowUpDown } from "lucide-react";
 import { useRef } from "react";
 // import Link from 'next/link'
 // import { ArrowUpDown, Network } from "lucide-react";
