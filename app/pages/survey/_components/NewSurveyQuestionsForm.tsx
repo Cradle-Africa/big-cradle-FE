82aaa4560eb27911194f4699b2fa8af47c7e5d5b
philippetsongo <philippetsongo90@gmail.com>
2025-07-22 18:41:58 +0200

    Integration of the suvey suggested question on survey creation, filter the survey list, integration of the survey data entries and filter

--- app/pages/survey/_components/NewSurveyQuestionsForm.tsx
+++ app/pages/survey/_components/NewSurveyQuestionsForm.tsx
@@ -240,7 +240,7 @@ const NewSurveyQuestionsForm = ({
             )}
             className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 hover:cursor-pointer transition"
           >
-            Get Suggested Questions
+            Get Suggested Reasearch/Survey Questions
           </button>
         </div>
 
