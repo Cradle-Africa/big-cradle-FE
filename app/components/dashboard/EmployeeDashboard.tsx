2b02b5b8669204db1e00fabd99205232c8e6f7bb
Optimus ged <gedeonyalakuhanda@gmail.com>
2025-07-28 17:03:21 +0200

    fix conflicts

--- app/components/dashboard/EmployeeDashboard.tsx
+++ app/components/dashboard/EmployeeDashboard.tsx
@@ -1,26 +1,14 @@
 import axios from "@/app/lib/axios";
 import { useFetchMe } from "@/app/shared/_features/hooks";
-<<<<<<< HEAD
-import { Grid, Spinner } from "@radix-ui/themes";
-import { ArrowDownUp, Database, Scan, UsersRound } from "lucide-react";
-import { useState } from "react";
-import DashboardCharts from "../charts/DashboardCharts";
-=======
 import { useState } from "react";
 import DashboardCharts from "../charts/DashboardCharts";
 import KycVerification from "../KycVerification";
 import { Grid, Spinner } from "@radix-ui/themes";
 import SentimentAnalysis from "../charts/SentimentAnalysis";
->>>>>>> 01ebbee1abfc4dce082a5aea426272af267bbf74
 import RespondersGrowth from "../charts/RespondersGrowth";
-import SentimentAnalysis from "../charts/SentimentAnalysis";
 import TopOrganization from "../charts/TopOrganization";
-<<<<<<< HEAD
-import KycVerification from "../KycVerification";
-=======
 import PlatformOverviewHeader from "../charts/PlatformOverviewHeader";
 import Summary from "../charts/Summary";
->>>>>>> 01ebbee1abfc4dce082a5aea426272af267bbf74
 
 const EmployeeDashboard = () => {
   const [openBusinessKycVerification, setOpenBusinessKycVerification] =
