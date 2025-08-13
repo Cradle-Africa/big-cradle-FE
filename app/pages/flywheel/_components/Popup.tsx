93baf1b59bd5b6bff81d4054c3c9c9cf9570a52d
philippetsongo <philippetsongo90@gmail.com>
2025-08-13 15:38:52 +0200

    add the connect api link on data flywheel

--- app/pages/flywheel/_components/Popup.tsx
+++ app/pages/flywheel/_components/Popup.tsx
@@ -1,9 +1,10 @@
-"use client";
+// "use client";
 
-import { ArrowUpDown } from "lucide-react";
+import { ArrowUpDown, Network } from "lucide-react";
 import { useRef } from "react";
 // import Link from 'next/link'
 // import { ArrowUpDown, Network } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 interface PopUpProps {
     openPopup: boolean;
@@ -13,6 +14,7 @@ interface PopUpProps {
 
 const PopUp: React.FC<PopUpProps> = ({ openPopup, onClose, onBuildPipeline }) => {
     const menuRef = useRef<HTMLDivElement>(null);
+    const router = useRouter();
 
     return (
         <div>
@@ -20,35 +22,50 @@ const PopUp: React.FC<PopUpProps> = ({ openPopup, onClose, onBuildPipeline }) =>
                 <div>
                     <div className="fixed inset-0 bg-black/40 z-10" onClick={onClose} />
                     <div
-                        className="text-center bg-white px-5 py-5 md:px-8 md:py-8 rounded-lg w-82 md:w-full max-w-2xl z-50 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                        className="text-center bg-white px-5 py-5 md:px-8 md:py-8 rounded-lg w-82 md:w-full max-w-3xl z-50 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
                         ref={menuRef}
                     >
 
-
+                        <h3 className="font-bold"> How would you like to collect your data?</h3>
                         <div className="md:flex justify-between gap-3 md:gap-5 mt-5">
-                            {/* <div className="md:w-1/2 md:h-44 px-6 py-6 bg-[#FCEBFF] hover:border hover:border-[#d27ce1] hover:transition rounded-lg cursor-pointer">
-                                <Link className="w-full h-full"
-                                href="/pages/flywheel/social-media"
+                            <div className="md:w-full bg-orange-50">
+                                <button
+                                    onClick={() => {
+                                        router.push("https://tinyurl.com/2ammll52")
+                                        onClose();
+                                    }}
+                                    className="md:mt-0 w-full md:h-48 px-6 py-6 border-2 border-dashed border-orange-400 hover:border  hover:transition-all ease-in rounded-lg cursor-pointer"
+
                                 >
                                     <div className="flex justify-center">
-                                        <div className="bg-white text-[#d27ce1] rounded-full p-3">
+                                        <div className="bg-white text-orange-400 rounded-full p-3">
                                             <Network className="" />
                                         </div>
                                     </div>
-                                    <p className="mt-3 text-md text-gray-800">Connect social media account</p>
-                                    <p className="text-sm mt-3">
-                                        Analyse your Social media account
+                                    <p className="text-lg font-bold mt-3">
+                                        Connect via Api
                                     </p>
-                                </Link>
-                            </div> */}
+                                    <p className="mt-3 text-md text-gray-800">Integrate with your existing apps, websites, or CRM</p>
+                                    
+                                </button>
+                                {/* <button
+                                    className="w-full mt-5 bg-blue-600 text-white px-5 py-2 rounded-md flex justify-center hover:cursor-pointer"
+                                    onClick={() => {
+                                        onBuildPipeline();
+                                        onClose();
+                                    }}
+                                >
+                                        Connect via Api
+                                </button> */}
+                            </div>
 
-                            <div className="md:w-full">
+                            <div className="md:w-full bg-blue-50">
                                 <button
                                     onClick={() => {
                                         onBuildPipeline();
                                         onClose();
                                     }}
-                                    className="md:mt-0 w-full md:h-44 px-6 py-6 border-2 border-dashed border-blue-500 hover:border  hover:transition-all ease-in rounded-lg cursor-pointer"
+                                    className="md:mt-0 w-full md:h-48 px-6 py-6 border-2 border-dashed border-blue-500 hover:border  hover:transition-all ease-in rounded-lg cursor-pointer"
 
                                 >
                                     <div className="flex justify-center">
@@ -56,20 +73,21 @@ const PopUp: React.FC<PopUpProps> = ({ openPopup, onClose, onBuildPipeline }) =>
                                             <ArrowUpDown className="" />
                                         </div>
                                     </div>
-                                    <p className="mt-3 text-md text-gray-800">How would you like to collect your data</p>
-                                    <p className="text-sm mt-3">
+                                    <h2 className="text-lg font-bold mt-3">
                                         Build your own data pipeline
-                                    </p>
+                                    </h2>
+                                    <p className="mt-3 text-md text-gray-800">Use our form builder to create your own data pipeline</p>
+                                    
                                 </button>
-                                <button
+                                {/* <button
                                     className="w-full mt-5 bg-blue-600 text-white px-5 py-2 rounded-md flex justify-center hover:cursor-pointer"
                                     onClick={() => {
                                         onBuildPipeline();
                                         onClose();
                                     }}
                                 >
-                                    Build your pipeline
-                                </button>
+                                    Build your own pipeline
+                                </button> */}
                             </div>
 
                         </div>
