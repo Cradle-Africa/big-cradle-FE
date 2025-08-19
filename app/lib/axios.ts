e106161a483f376120fc57669e621778a7713927
philippetsongo <philippetsongo90@gmail.com>
2025-08-19 19:44:04 +0200

    change the app url

--- app/lib/axios.ts
+++ app/lib/axios.ts
@@ -14,7 +14,7 @@ import { refreshTokenService } from "../services/user/userService";
 
 // export const BASE_URL = "https://big-cradle-be-dev.onrender.com/api/v1";
 export const BASE_URL = "https://big-cradle-be-1.onrender.com/api/v1";
-export const INTERNAL_URL = "https://big-cradle-frontend-eight.vercel.app";
+export const INTERNAL_URL = "https://app.bigcradle.com";
 
 const apiClient_ = axios.create({
 	baseURL: BASE_URL,
